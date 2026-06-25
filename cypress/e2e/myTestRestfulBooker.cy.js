87e115edf3ba1c86dbed9572e2bb58545482e344
Natalia <nataliacaporalec@gmail.com>
2026-06-25 07:05:15 -0300

    Test Se completa la reserva de una habitación con exito

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -4,7 +4,7 @@ describe('Test RestfulBooker', () => {
     cy.visit('https://automationintesting.online/', { failOnStatusCode: false });
   });
 
-// Test ADMIN
+  // Test ADMIN
   it('Navegar al panel de admin y loguearse con éxito', () => {
     cy.contains('a', 'Admin panel')
       .scrollIntoView()
@@ -14,27 +14,51 @@ describe('Test RestfulBooker', () => {
     cy.get('#username').type('admin');
     cy.get('#password').type('password');
     cy.get('button[type="submit"]').click();
-    cy.url().should('include', '/admin/rooms'); 
+    cy.url().should('include', '/admin/rooms');
   });
 
-// Test Rooms Catalog
+  // Test Rooms Catalog
   it('Hay habitaciones disponibles', () => {
     // Verifica que el contenedor general del listado sea visible.
     cy.contains('h2', 'Our Rooms').should('be.visible');
-// revisar!! el cy.get me parece horrible, pero no encontré otra forma de acceder a los elementos de las habitaciones. Si alguien tiene una mejor idea mejor
+    // revisar!! el cy.get me parece horrible, pero no encontré otra forma de acceder a los elementos de las habitaciones. Si alguien tiene una mejor idea mejor
 
     cy.get(':nth-child(1) > .card > .card-footer > .btn')
       .contains('Book now')
       .should('have.length.at.least', 1); // Verifica que haya como mínimo 1 habitación cargada
-});
+  });
 
- it('Acceder a las habitaciones disponibles desde la barra de navegación', () => {
-  // 1. Buscamos el botón "Rooms" en la barra de navegación y hacemos click
+  it('Acceder a las habitaciones disponibles desde la barra de navegación', () => {
+    // Buscamos el botón "Rooms" en la barra de navegación y hacemos click
     cy.contains('a', 'Rooms')
       .should('be.visible')
       .click();
-    // Verifica que el contenedor general del listado sea visible.
+    // Verificamos que el contenedor general del listado sea visible.
     cy.contains('h2', 'Our Rooms').should('be.visible');
 
+  });
+
+
+  it('Debería completar el formulario de reserva con datos válidos', () => {
+  
+    cy.abrirHabitacionNumero(1);
+    cy.get('.rbc-calendar').should('be.visible');
+
+    cy.seleccionarRangoDeFechasEnElCalendario(10, 12);
+
+     cy.get('button:contains("Reserve Now")').click();
+
+    cy.completarFormularioDeReservaConDatosValidos('Natalia', 'Caporale', 'natalia@ejemplo.com', '011234567891');
+    // Click en el botón de reserva para enviar el formulario
+    cy.get('button:contains("Reserve Now")').click();
+
+    //verificacion de la reserva
+    cy.get('.card-body')
+      .should('be.visible')
+      .and('contain.text', 'Booking Confirmed') // Verifica que el mensaje de confirmación esté presente 
+      .and('contain.text', 'Your booking has been confirmed for the following dates:');
+  });
 });
-});
+
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,4 +5,70 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   }
   // Si no es el error 418, dejamos que rompa.
   return true;
-})
+});
+
+Cypress.Commands.add('validarCalendarioHabitacion', (index) => {
+  // Agarra la habitacion que le corresponde, segun el index que se le pase como parámetro.
+  cy.get(`#rooms > .container > .row > :nth-child(${index})`)
+    .find('.card > .card-footer > .btn')
+    .should('be.visible')
+    .click();
+  // Valida que el calendario de reservas sea visible.
+  cy.get('.rbc-calendar')
+    .should('be.visible');
+
+});
+Cypress.Commands.add('obtenerListaHabitaciones', () => {
+  // Le pegamos a la API interna de Shady Meadows
+  return cy.request('https://automationintesting.online/room/')
+    .then((response) => {
+      // La API devuelve un objeto que contiene un array llamado 'rooms'
+      const listaHabitaciones = response.body.rooms;
+
+      // Devolvemos la lista completa para que la use el test
+      return listaHabitaciones;
+    });
+});
+
+Cypress.Commands.add('abrirHabitacionNumero', (index) => {
+  cy.get('#rooms > .container > .row > div')
+    .eq(0)
+    .find('.card > .card-footer > .btn')
+    .click();
+
+});
+
+Cypress.Commands.add('seleccionarRangoDeFechasEnElCalendario', (diaInicio, diaFin) => {
+  cy.get('.rbc-month-view .rbc-date-cell:not(.rbc-off-range)')
+    .should('have.length.at.least', 15)
+    .then(($days) => {
+      // Agarramos dos días de la segunda semana
+      const elementoInicio = $days.eq(10);
+      const elementoFin = $days.eq(12);
+
+      // Intentamos hacer click directo en el elemento interactivo (usualmente el botón interno)
+      // Si no tiene botón, el click con force: true en la celda debería registrarse
+      cy.wrap(elementoInicio).find('button, a').then(($btn) => {
+        if ($btn.length > 0) {
+          cy.wrap($btn).click({ force: true });
+        } else {
+          cy.wrap(elementoInicio).click({ force: true });
+        }
+      });
+      cy.wrap(elementoFin).find('button, a').then(($btn) => {
+        if ($btn.length > 0) {
+          cy.wrap($btn).click({ force: true });
+        } else {
+          cy.wrap(elementoFin).click({ force: true });
+        }
+      });
+    });
+});
+
+
+Cypress.Commands.add('completarFormularioDeReservaConDatosValidos', (firstName, lastName, email, phone) => {
+  cy.get('input[name="firstname"]').should('be.visible').type(firstName);
+  cy.get('input[name="lastname"]').type(lastName);
+  cy.get('input[name="email"]').type(email);
+  cy.get('input[name="phone"]').type(phone);
+});
