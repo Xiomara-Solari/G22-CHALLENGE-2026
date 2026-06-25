7254c9b4f82e30c5fe56a5a4cc9c98aadb8fa8ab
Xiomara-Solari <xiomarasolari@gmail.com>
2026-06-25 17:50:01 -0300

    Modulo 3.2 Validaciones del formulario de reserva

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -1,11 +1,11 @@
-describe('Test RestfulBooker', () => {
-  beforeEach(() => {
-    // Visitamos la página principal con la opción para ignorar códigos de estado erróneos
-    cy.visit('https://automationintesting.online/', { failOnStatusCode: false });
+describe('Test RestfulBooker', function() {
+  beforeEach(function() {
+    cy.fixture('bookingData').as('datos');
+    cy.visit('https://automationintesting.online/');
   });
 
   // Test ADMIN
-  it('Navegar al panel de admin y loguearse con éxito', () => {
+  it('Navegar al panel de admin y loguearse con éxito', function() {
     cy.contains('a', 'Admin panel')
       .scrollIntoView()
       .should('be.visible')
@@ -18,47 +18,49 @@ describe('Test RestfulBooker', () => {
   });
 
   // Test Rooms Catalog
-  it('Hay habitaciones disponibles', () => {
-    // Verifica que el contenedor general del listado sea visible.
+  it('Hay habitaciones disponibles', function() {
     cy.contains('h2', 'Our Rooms').should('be.visible');
-    // revisar!! el cy.get me parece horrible, pero no encontré otra forma de acceder a los elementos de las habitaciones. Si alguien tiene una mejor idea mejor
-
     cy.get(':nth-child(1) > .card > .card-footer > .btn')
       .contains('Book now')
-      .should('have.length.at.least', 1); // Verifica que haya como mínimo 1 habitación cargada
+      .should('have.length.at.least', 1);
   });
 
-  it('Acceder a las habitaciones disponibles desde la barra de navegación', () => {
-    // Buscamos el botón "Rooms" en la barra de navegación y hacemos click
+  it('Acceder a las habitaciones disponibles desde la barra de navegación', function() {
     cy.contains('a', 'Rooms')
       .should('be.visible')
       .click();
-    // Verificamos que el contenedor general del listado sea visible.
     cy.contains('h2', 'Our Rooms').should('be.visible');
-
   });
 
-
-  it('Debería completar el formulario de reserva con datos válidos', () => {
-  
+  it('Debería completar el formulario de reserva con datos válidos', function() {
     cy.abrirHabitacionNumero(1);
     cy.get('.rbc-calendar').should('be.visible');
 
     cy.seleccionarRangoDeFechasEnElCalendario(10, 12);
 
-     cy.get('button:contains("Reserve Now")').click();
-
-    cy.completarFormularioDeReservaConDatosValidos('Natalia', 'Caporale', 'natalia@ejemplo.com', '011234567891');
-    // Click en el botón de reserva para enviar el formulario
     cy.get('button:contains("Reserve Now")').click();
 
-    //verificacion de la reserva
+    cy.completarFormularioDeReservaConDatosValidos('Natalia', 'Caporale', 'natalia@ejemplo.com', '011234567891');
+    cy.get('button:contains("Reserve Now")').click();
+
     cy.get('.card-body')
       .should('be.visible')
-      .and('contain.text', 'Booking Confirmed') // Verifica que el mensaje de confirmación esté presente 
+      .and('contain.text', 'Booking Confirmed')
       .and('contain.text', 'Your booking has been confirmed for the following dates:');
   });
+
+  // Prueba 3.2: Validaciones del formulario de reserva (Camino Negativo)
+  it('Validar que el formulario de reserva muestre errores cuando se envía vacío', function() {
+    cy.abrirHabitacionNumero(1);
+    cy.get('.rbc-calendar').should('be.visible');
+    cy.seleccionarRangoDeFechasEnElCalendario(10, 12);
+    cy.get('button:contains("Reserve Now")').click();
+
+    cy.get('.book-room').click();
+
+    cy.get('.alert-danger').should('contain.text', 'must not be null');
+    cy.get('.alert-danger').should('contain.text', 'size must be between');
+
+    cy.get('.book-room').should('be.visible');
+  });
 });
-
-
-
